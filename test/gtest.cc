613241e8fa43cd33c0bb602500e2ed6655403c1a
Mark Friedenbach <mark@friedenbach.org>
2022-08-22 00:20:08 -0700

    Add make check using the googletest framework

---
+++ test/gtest.cc
@@ -0,0 +1,23 @@
+/* Copyright (c) 2022 Mark Friedenbach
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <gtest/gtest.h>
+
+TEST(gtest, assert_eq)
+{
+    ASSERT_EQ(0, 0);
+}
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+
+    return RUN_ALL_TESTS();
+}
+
+/* End of File
+ */
